6b447eba004ff0e99aef7c4bb2e4f78c4932fe0c
Matt R <mattreimer1@yahoo.com>
2020-12-07 15:25:00 -0600

    -m first exercise commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://img.icons8.com/emoji/2x/small-airplane.png';
+  image3 = 'https://img.icons8.com/cotton/2x/small-axe.png';
 
   constructor() { }
 
