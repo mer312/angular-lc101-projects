fc7f3b8ce61f122d7324768409b83121fb30b9df
Matt R <mattreimer1@yahoo.com>
2020-12-07 19:17:04 -0600

    "almost there"

--- lesson1/exercises/src/app/fav-links/fav-links.component.ts
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-links.component.css']
 })
 export class FavLinksComponent implements OnInit {
- favLinks: string []= ["google.com", "yahoo.com"];
+favLinks: string []= ["google.com", "yahoo.com"];
   constructor() { }
 
   ngOnInit() {
--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Fav Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
   image2 = 'https://img.icons8.com/emoji/2x/small-airplane.png';
   image3 = 'https://img.icons8.com/cotton/2x/small-axe.png';
